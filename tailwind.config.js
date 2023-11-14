759f6d9eefafa985a2e8b724744aca6b4a987e28
JaymeDehoyos <85977165+JaymeDehoyos@users.noreply.github.com>
2023-11-14 14:21:39 -0700

    Add to GitHub

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js, jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#8B0000'
+      }
+    },
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif'],
+    }
+  },
+  plugins: [],
+}
+
