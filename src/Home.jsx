e1b1b9207b1027912f2a093e67722cadad0922bd
Jayme Dehoyos <85977165+JaymeDehoyos@users.noreply.github.com>
2023-11-29 13:48:24 -0700

    small updates

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,5 @@
 import Safe from "./assets/safe_home.jpg"
-import Radon from "./assets/radon_home.jpg"
+// import Radon from "./assets/radon_home.jpg"
  
 const Header = () => {
 
@@ -7,25 +7,25 @@ const Header = () => {
     <div>
       <h3 className="text-6xl mt-10 font-bold text-center">Colorado Radon Testing</h3>
 
-      <div className="max-w-[1400px] h-[500px] bg-red-900 mx-auto my-20 pt-16 lg:mb-[20%] md:mb-[35%] px-4 grid lg:grid-cols-3 gap-4">
-        <div className="lg:top-20 relative lg:col-span-1 col-span-2">
-          <h1 className="text-2xl font-semibold">WELCOME</h1>
-          <p className="pt-4">We serve Denver Metro, Colorado Springs, and the Front Range area.</p>
+      <div className="flex justify-center my-32 mt-10">
+        <div className="text-center p-16 mx-12 rounded-xl shadow-xl">
+          <h1 className="text-4xl font-bold text-center">WELCOME</h1>
+          <p className="pt-4 text-black">We serve Denver Metro, Colorado Springs, and the Front Range area.</p>
           <p>Weather you are a homeowner, buying, selling, or renting you are at risk for Radon exposure in Colorado.</p>
           <p>Radon is the leading cause of lung cancer among nonsmokers in the United States.</p>
-          <a href="#Radon" className="hover:text-black hover:opacity-80 duration-150 text-white">Learn More </a>
+          <a href="#Radon" className="hover:text-black hover:opacity-80 duration-150 text-black">Learn More </a>
 
-          <div className="grid grid-cols-2 col-span-2 gap-2">
+          <div className=" float-left">
             <img 
-              className="row-span-2 object-cover w-full h-full"
+              className=""
               src={Safe}
               alt="Safe Home"
             />
-            <img
+            {/* <img
               className="object-cover w-full h-full"
               src={Radon}
               alt="How Radon comes in"
-            />
+            /> */}
 
           </div>
         </div>
--- src/Logos.jsx
+++ src/Logos.jsx
@@ -1,17 +1,10 @@
 // import React from "react";
-import AARST from "./assets/aarst.png";
 import NRPP from "./assets/NRPP.png";
 
 const Logos = () => {
   return (
     <div className="mx-auto mt-10 items-center">
       <div className="flex gap-8">
-        <img 
-          href="https://aarst.org"
-          className="h-20 w-auto hover:opacity-80 duration-150" 
-          src= {AARST}
-          alt="AARST"
-        />
         <img 
           href="https://nrpp.info"
           className="h-20 w-auto hover:opacity-80 duration-150" 
